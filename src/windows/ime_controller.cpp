992ff5bc9ec951d0eb6054d007e97304992e4062
Douyin-vbuser <douyin_vbuser@outlook.com>
2025-05-24 19:38:55 +0800

    rebuild windows

--- src/windows/ime_controller.cpp
+++ src/windows/ime_controller.cpp
@@ -1,11 +1,27 @@
 ﻿#include "com_vbuser_ime_IMEController.h"
 #include <windows.h>
+#include <Imm.h>
+#pragma comment(lib, "Imm32.lib")
 
-JNIEXPORT void JNICALL Java_com_vbuser_ime_IMEController_toggleIME
-(JNIEnv* env, jclass cls, jboolean enable) {
-    bool capsLockState = (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
-    if (capsLockState != enable) {
-        keybd_event(VK_CAPITAL, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
-        keybd_event(VK_CAPITAL, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+JNIEXPORT void JNICALL Java_com_vbuser_ime_IMEController_toggleIME(
+    JNIEnv* env, jclass cls, jboolean enable) {
+
+    HIMC hIMC = ImmGetContext(GetForegroundWindow());
+    if (hIMC == nullptr) {
+        return;
     }
+
+    DWORD dwConversion;
+    DWORD dwSentence;
+
+    if (ImmGetConversionStatus(hIMC, &dwConversion, &dwSentence)) {
+        if (enable) {
+            dwConversion |= IME_CMODE_NATIVE;
+        } else {
+            dwConversion &= ~IME_CMODE_NATIVE;
+        }
+        ImmSetConversionStatus(hIMC, dwConversion, dwSentence);
+    }
+
+    ImmReleaseContext(GetForegroundWindow(), hIMC);
 }
